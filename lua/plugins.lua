0daabfd82b5d122ca981a9c178397ad838871713
princebillygk <princebillygk@gmail.com>
2023-07-26 10:33:35 +0600

    Add coc-settings.json changes, nvim-tree, Spectre, and Gitsigns plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -25,4 +25,12 @@ return require('packer').startup(function(use)
     -- or                            , branch = '0.1.x',
     requires = { { 'nvim-lua/plenary.nvim' } }
   }
+
+  use {
+   'nvim-pack/nvim-spectre',
+    requires = { { 'nvim-lua/plenary.nvim' } }
+  }
+
+  use "sindrets/diffview.nvim"
+  use "lewis6991/gitsigns.nvim"
 end)
